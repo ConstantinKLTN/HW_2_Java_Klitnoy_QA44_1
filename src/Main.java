aedfeece97f64f8a153027d5a57828ff3f155d99
Constantin K <kiskiffa@gmail.com>
2022-08-16 21:04:50 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 1000; // потрачено на билеты
+
+        int Miles = price / 20; // Формула расчета миль
+
+
+        System.out.println("Спасибо,что Вы с нами, Вам начислено:");
+        System.out.println(Miles + " " + "Миль");
+
+    }
+}
